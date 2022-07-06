ff1d8b2e21f355a64f45ab64458e304a09c6031d
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-07-06 16:44:41 +0100

    filter inputs added

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ import { pokemonArray } from './data/pokemon.js';
 
 // Document query selector for card container 
 const body = document.querySelector("body")
+const pokemonName = document.querySelector("#pokemon-name")
+const numOfResults = document.querySelector("#num-of-results")
+const pokemonType = document.querySelector("#pokemon-type")
+const submitBtn = document.querySelector("#submit-btn")
 const container = document.querySelector(".card-container")
 
-//body.innerHTML += "";
-
 const entry = Object.entries(pokemonArray)
+
+const filterResults = () => {
+    console.log(pokemonName.value)
+    console.log(numOfResults.value)
+    console.log(pokemonType.value)
+}
 
 // Going through each Pokemon object 
 entry.forEach(e => {
     let pokemonName = e[0,1].name.charAt(0).toUpperCase() + e[0,1].name.slice(1);
     let pokemonTypes = "";
+    let pokemonId= e[0,1].id;
     let arrayOfTypes = (e[0,1].types);
 
     // Check for single or doubble Pokemon types to add "&"
@@ -28,7 +37,9 @@ entry.forEach(e => {
         <img id="card__image" src="${e[0,1].sprite}" alt="">
         <div class="card__content">
             <h2 id="card__heading">${pokemonName}</h2>
-            <p class="card__text">${pokemonName} (#${e[0,1].id}) is a ${pokemonTypes} type pokemon</p>
+            <p class="card__text">${pokemonName} (#${pokemonId}) is a ${pokemonTypes} type pokemon</p>
         </div>
     </div>`;
 });
+
+submitBtn.addEventListener("click", filterResults)
